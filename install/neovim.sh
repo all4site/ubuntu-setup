ca8ccb6b77907fbe0970c236d532e8ec23d2be44
all4site <all4sitecomua@gmail.com>
2023-07-12 21:54:21 +0300

    remove nvim installer

--- install/neovim.sh
+++ install/neovim.sh
@@ -2,4 +2,5 @@ wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage &&
 	sudo apt install -y fuse gcc ripgrep &&
 	sudo cp nvim.appimage /usr/bin/nvim &&
 	chmod u+x /usr/bin/nvim &&
+	rm nvim.appimage &&
 	git clone git@github.com:all4site/myneovim.git ~/.config/nvim
--- install/ohmyzsh.sh
+++ install/ohmyzsh.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
  # sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
      git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&

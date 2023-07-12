dd31b83b0f84a3b955e778d987599b00db2823fa
all4site <all4sitecomua@gmail.com>
2023-07-12 22:16:09 +0300

    create .confid dir

--- install/neovim.sh
+++ install/neovim.sh
@@ -1,6 +1,7 @@
 wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage &&
 	sudo apt install -y fuse gcc ripgrep &&
 	sudo cp nvim.appimage /usr/bin/nvim &&
-	chmod u+x /usr/bin/nvim &&
+	chmod +x /usr/bin/nvim &&
 	rm nvim.appimage &&
+	mkdir -p ~/.config &&
 	git clone git@github.com:all4site/myneovim.git ~/.config/nvim

79179e49c5a5f053a8e06d411c35110962f0ec12
all4site <all4sitecomua@gmail.com>
2023-07-16 20:41:19 +0300

    fix

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ git clone https://github.com/all4site/ubuntu-setup.git &&
 	cd ubuntu-setup/install &&
 	chmod +x *.sh &&
 	mkdir -p ~/.config &&
-	cp ../.p10k.zsh ~/ &&
 	./install.sh &&
 	cd ~/ &&
 	rm -rf ubuntu-setup &&

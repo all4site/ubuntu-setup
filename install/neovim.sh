ba1898738b81745c47a0f159b45652d4e94291c4
All4site <35457395+all4site@users.noreply.github.com>
2024-12-27 01:43:30 +0200

    Update neovim.sh chnge neovim configuration

--- install/neovim.sh
+++ install/neovim.sh
@@ -3,4 +3,4 @@ wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage &&
 	sudo cp nvim.appimage /usr/bin/nvim &&
 	sudo chmod +x /usr/bin/nvim &&
 	rm nvim.appimage &&
-	git clone https://github.com/all4site/myneovim.git ~/.config/nvim
+	git clone https://github.com/all4site/neovim.git ~/.config/nvim

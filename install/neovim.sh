2f6bddedf4e74a245e5f878001bd9ac4d69cc7e3
All4site <35457395+all4site@users.noreply.github.com>
2025-06-06 22:04:31 +0300

    Another lint to nvim

--- install/neovim.sh
+++ install/neovim.sh
@@ -1,6 +1,6 @@
-wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage &&
+wget https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.appimage &&
 	sudo apt install -y fuse gcc ripgrep &&
-	sudo cp nvim.appimage /usr/bin/nvim &&
+	sudo cp nvim-linux-x86_64.appimage /usr/bin/nvim &&
 	sudo chmod +x /usr/bin/nvim &&
-	rm nvim.appimage &&
+	rm nvim-linux-x86_64.appimage &&
 	git clone https://github.com/all4site/neovim.git ~/.config/nvim

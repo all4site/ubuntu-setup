b12bdb5ff51a7af87694b4e6225b588a0b217dd3
all4site <all4sitecomua@gmail.com>
2025-06-07 15:06:30 +0000

    fix

--- install/docker.sh
+++ install/docker.sh
@@ -4,6 +4,6 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt update -y
+sudo apt -y install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER

14b96a7420da4138aa3a93359370a12b02ec356f
all4site <all4sitecomua@gmail.com>
2025-06-07 15:07:58 +0000

    fix

--- install/docker.sh
+++ install/docker.sh
@@ -1,9 +1,9 @@
-sudo apt install apt-transport-https ca-certificates curl software-properties-common 
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common 
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update -y
-sudo apt -y install docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER

c1bb6b572ee5a50abe55215c065a8595a38e8340
All4site <35457395+all4site@users.noreply.github.com>
2025-06-24 17:32:35 +0300

    Update update.sh

--- install/update.sh
+++ install/update.sh
@@ -1,1 +1,1 @@
-sudo apt update -y && sudo apt upgrade -y && sudo apt install unzip build-essential curl -y
+sudo apt update -y && sudo apt upgrade -y && sudo apt install unzip build-essential curl jq -y

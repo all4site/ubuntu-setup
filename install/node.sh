2cb666dc4d96f15a63e24e1c16dace027e4e4b14
All4site <35457395+all4site@users.noreply.github.com>
2025-07-02 21:56:51 +0300

    Update node.sh

--- install/node.sh
+++ install/node.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+# Download and install Node.js:
+nvm install 22
